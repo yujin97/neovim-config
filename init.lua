c6a59ae36fd1eb1d59499515c62d9be9375f98dd
Eugene <eugene97@connect.hku.hk>
2024-03-21 22:15:32 +0900

    feat: configure minor settings

--- init.lua
+++ init.lua
@@ -737,6 +737,9 @@ require('lazy').setup({
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
     'folke/tokyonight.nvim',
     priority = 1000, -- make sure to load this before all the other start plugins
+    opts = {
+      transparent = true,
+    },
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
---
+++ lua/theprimeagen/set.lua
@@ -0,0 +1,12 @@
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.incsearch = true
+
+vim.opt.colorcolumn = '80'
