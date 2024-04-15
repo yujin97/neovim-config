09f29a99f3b68fea05446de79c9f3401dd56b2a1
Eugene <eugene97@connect.hku.hk>
2024-04-15 21:05:46 +0900

    feat: add git commit key mapping

--- lua/custom/plugins/fugitive.lua
+++ lua/custom/plugins/fugitive.lua
@@ -4,5 +4,8 @@ return {
     vim.keymap.set('n', '<leader>gs', function()
       vim.cmd 'topleft Git'
     end, { desc = '[G]it [S]atus' })
+    vim.keymap.set('n', '<leader>gc', function()
+      vim.cmd 'topleft Git commit'
+    end, { desc = '[G]it [C]ommit' })
   end,
 }
